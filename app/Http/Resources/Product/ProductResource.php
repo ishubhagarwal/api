3ceb9b6bc2a2573d90a233531cabcc0294140de0
shubham <developershubham@outlook.com>
2017-12-10 17:55:23 +0530

    Product resouces maniputed

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,12 @@ class ProductResource extends Resource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+            'discount' => $this->discount,
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'Not rated yes',
+            'href' => [
+                'review' => route('reviews.index',$this->id)
+            ]
         ];
     }
 }
