9206c393827020e689b6051ecf46a35f00c0d1ae
shubham <developershubham@outlook.com>
2017-12-10 23:33:04 +0530

    Review resource created

---
+++ app/Http/Resources/Review/ReviewResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Review;
+use App\Model\Product;
+use Illuminate\Http\Resources\Json\Resource;
+
+class ReviewResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'product_name' => (Product::find($this->product_id)->name),
+            'product_id' => $this->product_id,
+            'customer' => $this->customer,
+            'body' => $this->review,
+            'star' => $this->star
+            
+        ];
+    }
+}
