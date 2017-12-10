e334be1776b4114663598b82183d1fdb52dfed4b
shubham <developershubham@outlook.com>
2017-12-11 02:06:05 +0530

    Product Creation Api created with ProductRequest

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\Product\ProductCollection;
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
-use App\Http\Resources\Product\ProductResource;
-use App\Http\Resources\Product\ProductCollection;
 
 class ProductController extends Controller
 {
+
+
+    public function __construct(){
+        $this->middleware('auth:api')->except(['index','show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +42,22 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+        $product->detail = $request->description;
+        $product->save();
+
+        return response([
+
+            'data' => new ProductResource($product)
+
+        ]);
+
     }
 
     /**
